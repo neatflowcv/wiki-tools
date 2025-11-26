4cb5c6f9e61788a2b3a579d64a0282f04336b392
jinsu <biosvos@gmail.com>
2025-11-26 21:16:53 +0900

    wiki 분리

--- main.py
+++ main.py
@@ -1,71 +1,14 @@
-import aiohttp
 import argparse
 import asyncio
-from dataclasses import dataclass
+import json
 from dotenv import load_dotenv
 import os
+import wiki
 
 load_dotenv()
 
 wiki_api_url = os.getenv("WIKI_API_URL")
 wiki_api_key = os.getenv("WIKI_API_KEY")
-
-
-@dataclass
-class WikiPage:
-    id: str
-    path: str
-    title: str
-    locale: str
-
-    def is_parent_path(self, path: str) -> bool:
-        paths = self.path.split("/")
-        parents = path.split("/")
-        return paths[: len(parents)] == parents
-
-    def is_equal_path(self, path: str) -> bool:
-        return self.path == path
-
-
-async def list_pages(url: str, key: str) -> list[WikiPage]:
-    payload = {"query": "query { pages { list { id path title locale } } }"}
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            url,
-            headers={"Authorization": f"Bearer {key}"},
-            json=payload,
-        ) as resp:
-            resp.raise_for_status()
-            data = await resp.json()
-
-    pages = data.get("data", {}).get("pages", {}).get("list", [])
-    return [
-        WikiPage(
-            id=page["id"], path=page["path"], title=page["title"], locale=page["locale"]
-        )
-        for page in pages
-    ]
-
-
-async def move_page(url: str, key: str, page: WikiPage, new_path: str):
-    print(f"Moving page {page.id} {page.path} to {new_path}")
-    payload = {
-        "query": "mutation MovePage($id: Int!, $destPath: String!, $destLocale: String!) { pages { move(id: $id, destinationPath: $destPath, destinationLocale: $destLocale) { responseResult { succeeded message } } } }",
-        "variables": {
-            "id": page.id,
-            "destPath": new_path,
-            "destLocale": page.locale,
-        },
-    }
-    async with aiohttp.ClientSession() as session:
-        async with session.post(
-            url,
-            headers={"Authorization": f"Bearer {key}"},
-            json=payload,
-        ) as resp:
-            resp.raise_for_status()
-            data = await resp.json()
-            print(data)
 
 
 def parse_args() -> argparse.Namespace:
@@ -78,23 +21,29 @@ def parse_args() -> argparse.Namespace:
 
 
 async def main():
-    args = parse_args()
+    # args = parse_args()
 
     if not wiki_api_url or not wiki_api_key:
         raise RuntimeError("WIKI_API_URL 및 WIKI_API_KEY 환경 변수가 필요합니다.")
 
-    pages = await list_pages(wiki_api_url, wiki_api_key)
-    targets = [page for page in pages if page.is_parent_path(args.old_path)]
-    print(targets)
+    pages = await wiki.list_pages(wiki_api_url, wiki_api_key)
+    # targets = [page for page in pages if page.is_parent_path(args.old_path)]
+    print(
+        json.dumps(
+            [page.__dict__ for page in pages],
+            ensure_ascii=False,
+            indent=2,
+        )
+    )
 
-    for target in targets:
-        new_path = args.new_path + target.path[len(args.old_path) :]
+    # for target in targets:
+    #     new_path = args.new_path + target.path[len(args.old_path) :]
 
-        # pages 중에 new_path와 동일한 경로가 있는지 확인
-        if any(page.is_equal_path(new_path) for page in pages):
-            new_path = new_path + "_dup"
+    #     # pages 중에 new_path와 동일한 경로가 있는지 확인
+    #     if any(page.is_equal_path(new_path) for page in pages):
+    #         new_path = new_path + "_dup"
 
-        await move_page(wiki_api_url, wiki_api_key, target, new_path)
+    #     await wiki.move_page(wiki_api_url, wiki_api_key, target, new_path)
 
 
 if __name__ == "__main__":
---
+++ wiki.py
@@ -0,0 +1,59 @@
+from dataclasses import dataclass
+import aiohttp
+
+
+@dataclass
+class WikiPage:
+    id: str
+    path: str
+    title: str
+    locale: str
+
+    def is_parent_path(self, path: str) -> bool:
+        paths = self.path.split("/")
+        parents = path.split("/")
+        return paths[: len(parents)] == parents
+
+    def is_equal_path(self, path: str) -> bool:
+        return self.path == path
+
+
+async def list_pages(url: str, key: str) -> list[WikiPage]:
+    payload = {"query": "query { pages { list { id path title locale } } }"}
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url,
+            headers={"Authorization": f"Bearer {key}"},
+            json=payload,
+        ) as resp:
+            resp.raise_for_status()
+            data = await resp.json()
+
+    pages = data.get("data", {}).get("pages", {}).get("list", [])
+    return [
+        WikiPage(
+            id=page["id"], path=page["path"], title=page["title"], locale=page["locale"]
+        )
+        for page in pages
+    ]
+
+
+async def move_page(url: str, key: str, page: WikiPage, new_path: str):
+    print(f"Moving page {page.id} {page.path} to {new_path}")
+    payload = {
+        "query": "mutation MovePage($id: Int!, $destPath: String!, $destLocale: String!) { pages { move(id: $id, destinationPath: $destPath, destinationLocale: $destLocale) { responseResult { succeeded message } } } }",
+        "variables": {
+            "id": page.id,
+            "destPath": new_path,
+            "destLocale": page.locale,
+        },
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post(
+            url,
+            headers={"Authorization": f"Bearer {key}"},
+            json=payload,
+        ) as resp:
+            resp.raise_for_status()
+            data = await resp.json()
+            print(data)
